daece235b75099f00d47950698181999078a8f97
jflmao <jflmao@live.cn>
2023-09-21 16:29:21 +0800

    Update for_linux.sh

--- for_linux.sh
+++ for_linux.sh
@@ -12,18 +12,18 @@ echo "当前支持的 ChromeDriver 版本号为: $CHROMEDRIVER_VERSION"
 # 下载对应的 ChromeDriver 压缩包到临时目录 /tmp
 curl --silent --show-error --location --fail --retry 3 --output /tmp/chromedriver-linux64.zip "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$CHROMEDRIVER_VERSION/linux64/chromedriver-linux64.zip" 
 echo "ChromeDriver $CHROMEDRIVER_VERSION 下载完毕" 
-
+cd /tmp
 # 解压缩对应的 ChromeDriver
-unzip /tmp/chromedriver-linux64.zip 
+unzip /chromedriver-linux64.zip 
 echo "ChromeDriver $CHROMEDRIVER_VERSION 解压完毕" 
 ls
 # 删除下载的临时文件
-rm -rf /tmp/LATEST_RELEASE_${CHROME_VERSION}.txt
-rm -rf /tmp/chromedriver-linux64.zip 
+rm -rf LATEST_RELEASE_${CHROME_VERSION}.txt
+rm -rf chromedriver-linux64.zip 
 echo "删除临时文件完毕" 
 
 echo "把 ChromeDriver 二进制文件移动到用户可执行目录" 
-sudo mv /tmp/chromedriver-linux64 /usr/local/bin/chromedriver 
+sudo mv chromedriver-linux64 /usr/local/bin/chromedriver 
 
 echo "给 ChromeDriver 二进制文件添加执行权限" 
 sudo chmod +x /usr/local/bin/chromedriver 
