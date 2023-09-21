1be50f3911791231239f11e3054c9b2b3ef708cf
jflmao <jflmao@live.cn>
2023-09-21 15:55:03 +0800

    Update for_linux.sh

--- for_linux.sh
+++ for_linux.sh
@@ -1,20 +1,34 @@
 #!/usr/bin/env bash
 
+# 获取本地环境中 Chrome 的大版本号
 CHROME_VERSION=$(google-chrome --version | cut -f 3 -d ' ' | cut -d '.' -f 1) 
 echo "当前 Chrome 大版本号为: $CHROME_VERSION" 
+
+# 获取对应的 ChromeDriver 版本号
 curl --silent --show-error --location --fail --retry 3 --output /tmp/LATEST_RELEASE_${CHROME_VERSION}.txt "https://googlechromelabs.github.io/chrome-for-testing/LATEST_RELEASE_${CHROME_VERSION}" 
-CHROMEDRIVER_RELEASE=$(cat /tmp/LATEST_RELEASE_${CHROME_VERSION}.txt) 
-echo "当前支持的 ChromeDriver 版本号为: $CHROMEDRIVER_RELEASE" 
-curl --silent --show-error --location --fail --retry 3 --output /tmp/chromedriver_linux64.zip "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$CHROMEDRIVER_RELEASE/linux64/chromedriver-linux64.zip" 
-echo "ChromeDriver $CHROMEDRIVER_RELEASE 下载完毕" 
-cd /tmp 
-unzip chromedriver_linux64.zip 
-echo "ChromeDriver $CHROMEDRIVER_RELEASE 解压完毕" 
-rm -rf chromedriver_linux64.zip 
-echo "删除压缩包完毕" 
+CHROMEDRIVER_VERSION=$(cat /tmp/LATEST_RELEASE_${CHROME_VERSION}.txt) 
+echo "当前支持的 ChromeDriver 版本号为: $CHROMEDRIVER_VERSION" 
+
+# 下载对应的 ChromeDriver 压缩包到临时目录 /tmp
+curl --silent --show-error --location --fail --retry 3 --output /tmp/chromedriver-linux64.zip "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$CHROMEDRIVER_VERSION/linux64/chromedriver-linux64.zip" 
+echo "ChromeDriver $CHROMEDRIVER_VERSION 下载完毕" 
+
+# cd /tmp 
+# 解压缩对应的 ChromeDriver
+unzip /tmp/chromedriver-linux64.zip 
+echo "ChromeDriver $CHROMEDRIVER_VERSION 解压完毕" 
+
+# 删除下载的临时文件
+rm -rf /tmp/LATEST_RELEASE_${CHROME_VERSION}.txt
+rm -rf /tmp/chromedriver-linux64.zip 
+echo "删除临时文件完毕" 
+
 echo "把 ChromeDriver 二进制文件移动到用户可执行目录" 
-sudo mv chromedriver-linux64 /usr/local/bin/chromedriver 
+sudo mv /tmp/chromedriver-linux64 /usr/local/bin/chromedriver 
+
 echo "给 ChromeDriver 二进制文件添加执行权限" 
 sudo chmod +x /usr/local/bin/chromedriver 
+cd /tmp
+ls
 chromedriver --version
             
